daa9ed19fbee688edf6a27759ff35558be3f5a64
Koposov Seryozha <koposov.serezha@gmail.com>
2023-03-13 10:51:10 +0300

    set Fav

--- RecipeMVP/FavoriteViewController.swift
+++ RecipeMVP/FavoriteViewController.swift
@@ -11,8 +11,19 @@ class FavoriteViewController: UIViewController {
     
     var presenter: FavoritePresenter!
 
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
+        setConstraints()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
         view.backgroundColor = .white
 
     }
@@ -20,4 +31,33 @@ class FavoriteViewController: UIViewController {
 
 extension FavoriteViewController: FavoriteViewProtocol {
     
+}
+
+extension FavoriteViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        cell.textLabel?.text = "fwe"
+
+        return cell
+    }
+
+
+}
+
+extension FavoriteViewController {
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+
+        ])
+    }
 }
